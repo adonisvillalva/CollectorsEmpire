b4f7e28563e908824e661bc6ef26e72cedc7aae3
LichKiller <adonis.villalva@outlook.com>
2023-04-12 00:51:21 -0500

    v 0.2.42

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ Si deseas ver el portal puedes visitar: [Collector's empire](www.collectors-empi
 -   v 0.2.3  Inicio sección de categorías.  
 -   v 0.2.4  Cambio de fuente de texto.
 -   v 0.2.41 Cambio de API.
+-   v 0.2.42 Cambio de API, instalación de boostrap y diseño de post internos.
--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,17 +1,16 @@
 import { fetchSinglePost } from "@/app/api/apiRest";
+import styles from '@/styles/posts/post.module.scss'
 export default async function Post({params}){
     const {id} = params;
     const post = await fetchSinglePost(id);
     return(
         <>
             {post.map(post => (
-                <article key={post.nid}>
-                    <h2>{post.title}</h2>
+                <article key={post.nid} className={styles.post}>
+                    <h2 className={styles.title}>{post.title}</h2>
                     <div dangerouslySetInnerHTML={{__html: post.body}}></div>
-                    <div dangerouslySetInnerHTML={{__html: post.field_img_texto}}></div>
                 </article>        
             ))}
         </>
-        
     )
 }
